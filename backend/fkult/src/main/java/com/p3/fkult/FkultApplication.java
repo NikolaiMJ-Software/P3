9ce0d9521eed73e04a750603a3a5a4ed76eda6e1
Kabuum <thomsenribermartin@gmail.com>
2025-10-15 14:06:09 +0200

    react and springboot initialization

---
+++ backend/fkult/src/main/java/com/p3/fkult/FkultApplication.java
@@ -0,0 +1,13 @@
+package com.p3.fkult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FkultApplication.class, args);
+    }
+
+}
---
+++ backend/fkult/src/test/java/com/p3/fkult/FkultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3.fkult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
