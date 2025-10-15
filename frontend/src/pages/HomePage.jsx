28d0e1b2f6d9a30bd80a220cead4b92d0b9d7620
Kabuum <thomsenribermartin@gmail.com>
2025-10-15 20:46:33 +0200

    Navigation example

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
+import {useNavigate} from "react-router-dom";
+
 export default function HomePage() {
-    return <h1>Welcome to the Home Page</h1>;
+    const navigate = useNavigate();
+    const goToMessages = () => {
+        navigate("/messages");
+    };
+
+    return <>
+        <h1>Welcome to the Home Page</h1>
+        <button onClick={goToMessages}>Messages</button>
+    </>
 }
