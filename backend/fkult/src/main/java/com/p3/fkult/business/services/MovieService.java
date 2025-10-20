744d1e692728196bce73dd1aa3ffd2f794ef12fc
Kabuum <thomsenribermartin@gmail.com>
2025-10-20 16:19:27 +0200

    WIP movie Poster rich preview

--- backend/fkult/src/main/java/com/p3/fkult/business/services/MovieService.java
+++ backend/fkult/src/main/java/com/p3/fkult/business/services/MovieService.java
@@ -5,11 +5,16 @@ import com.p3.fkult.persistence.entities.Movie;
 import com.p3.fkult.persistence.entities.Theme;
 import com.p3.fkult.persistence.entities.ThemeMovie;
 import com.p3.fkult.persistence.repository.*;
+import com.p3.fkult.presentation.controllers.MovieRequest;
 import com.p3.fkult.presentation.controllers.ThemeRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,8 +26,8 @@ public class MovieService {
     public MovieService(MovieRepository movieRepository){
         this.movieRepository = movieRepository;
     }
-//WIP should return a MovieDTO defined in controller folder
-    public void getMoviesByIds(List<Long> movieIds){
+
+    public List<MovieRequest> getMoviesByIds(List<Long> movieIds){
         List<Movie> movies = movieIds
                 .stream()
                 .map(movieId -> movieRepository.findById(movieId))
@@ -31,13 +36,27 @@ public class MovieService {
                 .stream()
                 .map(movie -> getPosterURL(movie))
                 .toList();
-
+        //WIP
+        return new ArrayList<>();
     }
+
     public String getPosterURL(Movie movie){
         String tConst = movie.getTconst() + "/";
         String movieURL = IMDB_URL + tConst;
 
-        return new String("wat");
+        try{
+            String html = fetchHTML(movieURL);
+            Document doc = Jsoup.parse(html);
+            Element ogImage = doc.selectFirst("meta[property=og:image]");
+            if (ogImage != null){
+                return ogImage.attr("content");
+            } else {
+                return null;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public String fetchHTML(String imdbURL){
---
+++ backend/fkult/src/main/java/com/p3/fkult/presentation/controllers/MovieRequest.java
@@ -0,0 +1,4 @@
+package com.p3.fkult.presentation.controllers;
+
+public class MovieRequest {
+}
