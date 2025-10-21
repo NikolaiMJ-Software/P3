156d1b49ca13f6dea4abab5b69b0a8c4b3d22d94
NikolaiMJ-Software <nikolai456654@gmail.com>
2025-10-21 14:36:17 +0200

    fixed the data leak, your welcome Martin ;D

--- backend/fkult/src/main/resources/data.sql
+++ backend/fkult/src/main/resources/data.sql
@@ -7,10 +7,14 @@ VALUES (4900, 'Martin', 'Kabuum', 0, 0);
 INSERT OR REPLACE INTO theme (id, name, user_id)
 VALUES (9999, 'Pirates Night', 4900);
 
+INSERT OR REPLACE INTO movie (id, tconst, movie_name, original_movie_name, year, runtime_minutes, is_active)
+VALUES 
+(999998, NULL, 'Gruppe 6 4 life', 'Gruppe 6 4 life', 2025, 120, 1), 
+(999999, NULL, 'Heyo hows it going', 'Heyo hows it going', 2025, 120, 1);
 
 -- Associate movies with this theme
-INSERT OR REPLACE INTO theme_movie (theme_id, movie_id) VALUES (9999, 170478);
-INSERT OR REPLACE INTO theme_movie (theme_id, movie_id) VALUES (9999, 188867);
+INSERT OR REPLACE INTO theme_movie (theme_id, movie_id) VALUES (9999, 999998);
+INSERT OR REPLACE INTO theme_movie (theme_id, movie_id) VALUES (9999, 999999);
 
 -- Add drinking rules
 INSERT OR REPLACE INTO drinking_rule (theme_id, rule_text)
