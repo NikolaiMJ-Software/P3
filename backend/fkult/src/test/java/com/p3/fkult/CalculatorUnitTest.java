2c2140ccdd9add7228d547588596a9fd4a8e07c1
Sebastianplaygames <94458257+Sebastianplaygames@users.noreply.github.com>
2025-10-21 11:31:17 +0200

    first unit test for imdbimport (more to be added soon)

--- backend/fkult/src/test/java/com/p3/fkult/CalculatorUnitTest.java
+++ backend/fkult/src/test/java/com/p3/fkult/CalculatorUnitTest.java
@@ -10,7 +10,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.p3.fkult.testTemplate.*;
@@ -31,7 +30,6 @@ class GreetingControllerTest {
 }
 
 
-@SpringBootTest
 public class CalculatorUnitTest {
 
     @Test
