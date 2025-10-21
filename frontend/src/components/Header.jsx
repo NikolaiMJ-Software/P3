9c7e83f41848c48dc72301d56318c713243bc4a2
mikkelgan <mikkelgan@live.dk>
2025-10-21 15:37:51 +0200

    Header with no CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,57 @@
 import logoPNG from '../assets/logo.png';
+import pizzaPNG from '../assets/pizza.png'
+import discordWEBP from '../assets/discord.webp'
+import {useNavigate} from "react-router-dom";
 
 export default function Header(){
-    // F-Kult logo to go to homepage
-    let logo = <button>
-        <img src={logoPNG} />
-    </button>
-    // F-Kult
-    // Pizza, Discord, User widgets
-    return logo;
+    const navigate = useNavigate();
+
+    return (
+        <header className="Header">
+            {/* F-Kult logo to go to homepage */}
+            <LogoButton onClick={()=>navigate("/")}/>
+
+            {/*  F-Kult */}
+            <h1>F-Kult</h1>
+
+            {/*  Pizza, Discord, User widgets */}
+            <NavButton
+                label="Pizza"
+                icon={<img src={pizzaPNG}/>}
+                onClick={() => {window.location.href = "https://f-kult-pizza-bestiller.vercel.app/"}}
+            />
+
+            <NavButton
+                label="Discord"
+                icon={<img src={discordWEBP}/>}
+                onClick={() => {console.log("discord!")}}
+            />
+
+            <NavButton
+                label="User"
+                icon={<img src={discordWEBP}/>}
+                onClick={() => {console.log("User!")}}
+            />
+        </header>
+    )
+}
+
+function LogoButton({ onClick }) {
+    return (
+        <button onClick={onClick}>
+            <img src={logoPNG} />
+        </button>
+    );
+}
+
+function NavButton({ icon, label, onClick }) {
+    return (
+        <div>
+            <button onClick={onClick} title={label}>
+                {icon}
+            </button>
+            {label}
+        </div>
+
+    );
 }
