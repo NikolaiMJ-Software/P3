b0a9f2474a2d2d728c4bd06af89633db1463d8ad
Kabuum <thomsenribermartin@gmail.com>
2025-11-04 15:16:25 +0100

    movie searching on frontend me and seb

--- frontend/src/services/movieService.jsx
+++ frontend/src/services/movieService.jsx
@@ -18,3 +18,27 @@ export async function getMoviesByTconsts(movieTconsts) {
     });
     return response.json();
 }
+
+export async function searchMovies(query, page, limit){
+    const response = await fetch(`${API_URL}/search?q=${encodeURIComponent(query)}&page=${page}&limit=${limit}`, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+    });
+    if (!response.ok){
+        throw new Error('Failed to count movies: ${response.status}');
+    }
+    return response.json();
+}
+
+export async function getMovieCount(query){
+    const response = await fetch(`${API_URL}/search/count?q=${encodeURIComponent(query)}`, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+    });
+    return response.json();
+}
+export async function getMoviePoster(tConst){
+    const response = await fetch(`${API_URL}/poster/${tConst}`);
+    if (!response.ok) throw new Error("Failed to fetch poster");
+    return await response.text(); // your endpoint returns string
+}
