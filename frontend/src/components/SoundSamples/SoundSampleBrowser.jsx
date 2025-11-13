e036ba1210b535eb701d75af4702194d62973c87
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2025-11-13 15:39:41 +0100

    layout change

--- frontend/src/components/SoundSamples/SoundSampleBrowser.jsx
+++ frontend/src/components/SoundSamples/SoundSampleBrowser.jsx
@@ -30,38 +30,21 @@ export default function SoundSampleBrowser() {
     return (
     <div className={"p-10"}>
         <div className={"w-full max-w-full h-fit border-2 border-text-primary rounded-3xl p-8"}>
-            {/* Upcoming themes card container */}
-            <p className={"m-4 font-bold"}>Uploaded soundsamples</p>
-            <div className="flex flex-col">
-                <UplaodedSoundSamples soundSamples={soundSamples}/>
+
+            {/* Users sound sample card container */}
+            <p className={"m-4 font-bold"}>Your sound samples</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                <UplaodedSoundSamples soundSamples={usersSS}/>
             </div>
 
             <div className={"border-1 m-8"}></div>
 
-            <p className={"m-4 font-bold"}>Your sound samples</p>
-            {/* Your themes card container */}
-            <div className={"pt-4 pl-6 flex row-end-5 flex gap-5"}>
-                {/* individual cards */}
-                <div className={"flex gap-5 p-4 overflow-x-auto"}>
-                    {/* individual cards */}
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>Create theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                    <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>theme</div>
-                </div>
+            {/* Upcoming themes card container */}
+            <p className={"m-4 font-bold"}>Uploaded soundsamples</p>
+            <div className="w-full max-w-full h-fit">
+                <UplaodedSoundSamples soundSamples={soundSamples}/>
             </div>
+
         </div>
     </div>
     )
--- frontend/src/components/SoundSamples/SoundSamplesCollection.jsx
+++ frontend/src/components/SoundSamples/SoundSamplesCollection.jsx
@@ -2,16 +2,15 @@ export default function UplaodedSoundSamples({ soundSamples }) {
 
     // Return sound sample cards
     return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        <>
             {soundSamples.map(soundSample =>  {
-               return <div className={"w-60 h-80 border-2 border-text-primary" +
-                        "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>
+               return <div className={"w-60 h-80 border-2 border-text-primary" + "text-lg font-medium shadow-sm hover:shadow-md transition shrink-0"}>
                     <div className={"flex gap-5 p-4 overflow-x-auto"}>
                         <h2 className={"text-sm text-text-secondary"}>{soundSample.username}</h2>
                         <p>{soundSample.soundSample}</p>
                     </div>
                 </div>
             })}
-        </div>
+        </>
     )
 }
