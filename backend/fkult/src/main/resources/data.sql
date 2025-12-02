fb44e86c05619f3cf72dbfab873084a5b94f8292
Nikolai Medom Jensen <nikolai456654@gmail.com>
2025-12-02 14:38:33 +0100

    removed ss from test data

--- backend/fkult/src/main/resources/data.sql
+++ backend/fkult/src/main/resources/data.sql
@@ -2,7 +2,7 @@
 -- Insert dummy user
 INSERT OR IGNORE INTO user (id, name, username, is_banned, is_admin)
 VALUES
-    (1, 'John Test', 'tester', 0, 1 ),
+(1, 'John Test', 'tester', 0, 1 ),
 (2, 'Mig Nielsen', 'Mig1', 0, 0),
 (3, 'Mig Jensen', 'mig2', 0, 0),
 (4, 'test Testsen', 'test', 0, 0),
@@ -29,29 +29,6 @@ VALUES
 (1,1, 'Drink when someone says "pirate"'),
 (2,1, 'Take a sip whenever a ship appears on screen');
 
--- Dummy sound samples
-INSERT OR IGNORE INTO sound_samples (link, file_path, user_id)
-VALUES 
-(NULL, '/film/starwars', 1),
-('https://www.example.com/audio/intro.mp3', Null, 2),
-(NULL, '/nature/rain', 3),
-('https://cdn.example.com/sound/birds.mp3', Null, 3),
-(NULL, '/music/rock_guitar', 2),
-(Null, '/studio/drums', 4),
-(NULL, '/mix/testtrack', 5),
-('https://sound.example.com/mixdown/final', Null, 5),
-('https://www.example.com/effects/laser.mp3', NULL, 3),
-(NULL, '/ambient/forest', 3),
-('https://cdn.example.com/sound/wind.mp3', NULL, 1),
-(NULL, '/sfx/explosion', 5),
-('https://media.example.com/sample/piano', NULL, 5),
-(NULL, '/music/classic', 1),
-('https://www.google.dk/tik-tok.com/1', NULL, 1),
-(NULL, '/studio/bass', 4),
-('https://cdn.example.com/sound/drums_loop.mp3', NULL, 5),
-(NULL, '/mix/track1', 5),
-('https://media.example.com/sample/guitar', NULL, 2),
-(NULL, '/nature/waterfall', 3);
 
 -- Real startup day
 INSERT OR IGNORE INTO event (id,event_date, theme_id)
